dcdd56f5f634b39826d62cd37d0aac2de95706a9
Naskar2002 <sourovnaskar212gmail.com>
2025-06-24 14:44:57 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,98 @@
+// aos initialization
+AOS.init();
+
+
+// Nav-bar
+
+const navCollapse = document.querySelector(".nav-collapse");
+const menu = document.querySelector(".ham");
+
+menu.addEventListener("click", () => {
+  navCollapse.classList.toggle("active");
+});
+
+// Banner-swiper
+
+const bannerSwiper = new Swiper("#banner", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  effect: "fade",
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+
+// testimonialSwiper
+
+const swiper = new Swiper("#testimonial", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 3,
+  spaceBetween: 12,
+  loop: true,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+  },
+  breakpoints: {
+    680: {
+      slidesPerView: 2,
+      spaceBetween: 8,
+    },
+    576: {
+      slidesPerView: 1,
+      spaceBetween: 8,
+    },
+    // 425: {
+    //   slidesPerView: 1,
+    //   spaceBetween: 7,
+    // },
+    // 320: {
+    //   slidesPerView: 1,
+    //   spaceBetween: 5,
+    // },
+  },
+});
+
+// blog-section-swiper
+
+const blogSwiper = new Swiper("#blog", {
+  direction: "horizontal",
+  slidesPerView: 3,
+  spaceBetween: 24,
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+  },
+  breakpoints: {
+    680: {
+      slidesPerView: 2,
+      spaceBetween: 8,
+    },
+    576: {
+      slidesPerView: 1.2,
+      spaceBetween: 8,
+    },
+    425: {
+      slidesPerView: 1.2,
+      spaceBetween: 7,
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 5,
+    },
+  },
+});
+
